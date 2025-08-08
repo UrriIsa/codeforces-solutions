0eeeb39c6487c3944e63e6e4801f2ba113fdea53
UrriIsa <isaaurrru@gmail.com>
2025-08-07 20:16:11 -0600

    refactor : cambio de carpeta.

---
+++ soluciones/Div2_A/228A.c
@@ -0,0 +1,50 @@
+/**
+ * id: 228A
+ * title: Is your horseshoe on the other hoof?
+ * tags: implementation, *800
+ */
+
+#include <stdio.h>
+
+int main(){
+    int nums[4], num = 0 ;
+    
+    scanf("%d %d %d %d", &nums[0], &nums[1] , &nums[2] , &nums[3]) ;
+
+    for(int i = 0 ; i < 4 ; i ++){
+        int aps = 0; 
+        for(int j = i + 1 ; j < 4 ; j++ ){
+            if(nums[i] == nums[j]) { 
+                num++; 
+                break; 
+            }
+        }
+    }
+
+    printf("%d\n", num);
+    return 0;
+}
+
+/*
+#include <stdio.h>
+#include <glib.h>
+
+int main() {
+    int nums[4], num = 0;
+    GHashTable *seen = g_hash_table_new(g_int_hash, g_int_equal); // Mapa de enteros
+
+    for (int i = 0; i < 4; i++) {
+        scanf("%d", &nums[i]);
+        if (g_hash_table_contains(seen, &nums[i])) {
+            num++;
+        } else {
+            g_hash_table_insert(seen, &nums[i], GINT_TO_POINTER(1));
+        }
+    }
+
+    printf("%d\n", num);
+    g_hash_table_destroy(seen); // Liberar memoria
+}
+
+
+*/
